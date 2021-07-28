36c737d0030ba73e342406e7a1ac82cdca86bf7e
mervoCS <mervoCS@gmail.com>
2021-07-28 17:48:56 -0400

    Fixed icon, language attribute, still needs some cleaning

--- app/src/main/java/com/myhouse/quizappsummer21/MainActivity.java
+++ app/src/main/java/com/myhouse/quizappsummer21/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         //initialize origin variables
@@ -164,6 +165,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    
 
 }
